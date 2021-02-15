068b40aef95658e7aafb7fc9f7cd90e64d297537
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-15 15:39:38 -0600

    my first react code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+// if node:
+// const HelloCulen = require(./HelloCullen)
+
+// Client side (react) we do:
+import HelloCullen from './HelloCullen';
+
+// App is a Component
+// React is made up of Components
+// Components are functions that return JSX
 function App() {
+  let name = 'Patrick';
+
+  let myH1 = <h1>How should I react? - {name}</h1>;
+
+  // "HTML" is actually "JSX"
+  // "JSX" = Javascript eXtended
+  // as in App.jsx
   return (
     <div className="App">
+      {myH1}
       <header className="App-header">
+        {/* helloCullen is a React Component*/}
+        <HelloCullen />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/HelloCullen.jsx
@@ -0,0 +1,13 @@
+function HelloCullen() {
+  return (
+    <>
+      <h1>Hello Cullen!</h1>
+      <h2>Sup?</h2>
+    </>
+  );
+}
+// In node we'd do:
+// module.exports = HelloCullen;
+
+// Client side (react) we do:
+export default HelloCullen;
